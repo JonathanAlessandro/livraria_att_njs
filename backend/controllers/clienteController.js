0384ce389792d6331b9ca756a613121712b180dc
JonathanAlessandro <jonathan.alessandro212@gmail.com>
2026-05-27 22:18:57 -0300

    adicionado try catch aos controllers e adicionado controllers de compra,editora,livro,categoria

--- backend/controllers/clienteController.js
+++ backend/controllers/clienteController.js
@@ -2,104 +2,128 @@ import clientesModel from "../models/clienteModel.js";
 
 class ClientesController {
     async getAllClientes(req, res) {
-        const allClientes = await clientesModel.showClientes();
-        if (allClientes.length === 0) {
-            return res.json({
-                message: "Nenhum usuário encontrado!",
-            });
+        try {
+            const allClientes = await clientesModel.showClientes();
+            if (allClientes.length === 0) {
+                return res.json({
+                    message: "Nenhum usuário encontrado!",
+                });
+            }
+            return res.json(allClientes);
+        } catch (error) {
+            return res.json({ message: `ocorreu um erro: ${error}` })
         }
-        return res.json(allClientes);
     }
 
     async getClienteById(req, res) {
 
-        const id = Number(req.params.id);
-        const findCliente = await clientesModel.getClienteById(id);
-        if (!findCliente) {
-            return res.status(404).json();
+        try {
+            const id = Number(req.params.id);
+            const findCliente = await clientesModel.getClienteById(id);
+            if (!findCliente) {
+                return res.status(404).json();
+            }
+            return res.json(findCliente);
+        } catch (error) {
+            return res.json({ message: `ocorreu um erro: ${error}` })
         }
-        return res.json(findCliente);
     }
 
     async clienteByEmail(req, res) {
-        const { email } = req.body;
-        const findEmail = await clientesModel.getClienteByEmail(email);
+        try {
+            const { email } = req.body;
+            const findEmail = await clientesModel.getClienteByEmail(email);
 
-        if (findEmail?.email === email) {
-            return res.json({
-                message: "Email já cadastrado!",
-            });
+            if (findEmail?.email === email) {
+                return res.json({
+                    message: "Email já cadastrado!",
+                });
+            }
+            return res.json({ email: findEmail.email })
+        } catch (error) {
+            return res.json({ message: `ocorreu um erro: ${error}` })
         }
-        return res.json({ email: findEmail.email })
     }
 
     async storeCliente(req, res) {
-        const { nome, email, telefone, cidade, estado } = req.body;
+        try {
+            const { nome, email, telefone, cidade, estado } = req.body;
 
-        if (nome === "" || !email || !telefone || !cidade || !estado) {
-            return res.json({ message: "Todos os campos são Obrigatórios" });
+            if (nome === "" || !email || !telefone || !cidade || !estado) {
+                return res.json({ message: "Todos os campos são Obrigatórios" });
+            }
+
+            const findEmail = await clientesModel.getClienteByEmail(email);
+
+            if (findEmail?.email === email) {
+                return res.json({
+                    message: "Email já cadastrado!",
+                });
+            }
+
+            const createCliente = await clientesModel.createCliente(req.body);
+
+            if (createCliente.affectedRows === 0) {
+                return res.json({
+                    message: "Não foi possível realizar o cadastro!",
+                });
+            }
+
+            return res.status(201).json();
+        } catch (error) {
+            return res.json({ message: `ocorreu um erro: ${error}` })
         }
-
-        const findEmail = await clientesModel.getClienteByEmail(email);
-
-        if (findEmail?.email === email) {
-            return res.json({
-                message: "Email já cadastrado!",
-            });
-        }
-
-        const createCliente = await clientesModel.createCliente(req.body);
-
-        if (createCliente.affectedRows === 0) {
-            return res.json({
-                message: "Não foi possível realizar o cadastro!",
-            });
-        }
-
-        return res.status(201).json();
     };
 
     async updateCLienteById(req, res) {
-        const id = Number(req.params.id);
-        const { nome, email, telefone, cidade, estado } = req.body;
-        const findEmail = await clientesModel.getClienteByEmail(email);
+        try {
+            const id = Number(req.params.id);
+            const { nome, email, telefone, cidade, estado } = req.body;
+            const findEmail = await clientesModel.getClienteByEmail(email);
 
 
-        if (!nome || !email || !telefone || !cidade || !estado) {
-            return res.json({ message: "Todos os campos são Obrigatórios" });
-        }
+            if (!nome || !email || !telefone || !cidade || !estado) {
+                return res.json({ message: "Todos os campos são Obrigatórios" });
+            }
 
-        if (findEmail?.email === email) {
+            if (findEmail?.email === email) {
+                return res.json({
+                    message: "Email já cadastrado!",
+                });
+            }
+
+            const updateCliente = await clientesModel.updateCliente(id, req.body);
+
+            if (updateCliente.affectedRows === 0) {
+                return res.json({
+                    message: "Não foi possível realizar a atualização!",
+                });
+            }
+
             return res.json({
-                message: "Email já cadastrado!",
+                message: "Cadastro realizado com Sucesso!",
             });
+        } catch (error) {
+            return res.json({ message: `ocorreu um erro: ${error}` })
         }
-
-        const updateCliente = await clientesModel.updateCliente(id, req.body);
-
-        if (updateCliente.affectedRows === 0) {
-            return res.json({
-                message: "Não foi possível realizar a atualização!",
-            });
-        }
-
-        return res.json({
-            message: "Cadastro realizado com Sucesso!",
-        });
     }
 
-    async removeCliente(req,res){
-        const id = Number(req.params.id);
-        const deleteCliente = await clientesModel.deleteCliente(id);
+    async removeCliente(req, res) {
+        try {
+            const id = Number(req.params.id);
+            const deleteCliente = await clientesModel.deleteCliente(id);
 
-        if (deleteCliente.affectedRows === 0) {
+            if (deleteCliente.affectedRows === 0) {
+                return res.json({
+                    message: "Não foi possível realizar a exclusão!",
+                });
+            }
             return res.json({
-                message: "Não foi possível realizar a exclusão!",
+                message: "Cadastro excluído com Sucesso!",
             });
+        } catch (error) {
+            return res.json({ message: `ocorreu um erro: ${error}` })
         }
-        return res.json({
-            message: "Cadastro excluído com Sucesso!",
-        });
     }
 }
 
--- backend/controllers/editoraController.js
+++ backend/controllers/editoraController.js
@@ -1,37 +1,76 @@
-import { message } from "statuses";
 import editoraModel from "../models/editoraModel";
 
-class EditoraController{
+class EditoraController {
     async getAllEditoras(req, res) {
-        const allEditoras = await editoraModel.showEditoras();
-        if (allEditoras.length === 0) {
-            return res.status(404).json();
+        try {
+            const allEditoras = await editoraModel.showEditoras();
+            if (allEditoras.length === 0) {
+                return res.status(404).json();
+            }
+            return res.json(allEditoras);
+        } catch (error) {
+            return res.json({ message: `ocorreu um erro: ${error}` })
         }
-        return res.json(allEditoras);
     }
 
     async getEditoraById(req, res) {
-        const id = Number(req.params.id);
-        const findEditora = await editoraModel.getEditoraById(id);
-        if (!findEditora) {
-            return res.status(404).json();
+        try {
+            const id = Number(req.params.id);
+            const findEditora = await editoraModel.getEditoraById(id);
+            if (!findEditora) {
+                return res.status(404).json();
+            }
+            return res.json(findEditora);
+        } catch (error) {
+            return res.json({ message: `ocorreu um erro: ${error}` })
         }
-        return res.json(findEditora);
     }
 
-    async addEditora(req,res){
-        const { nome, email, telefone, data_cadastro } = req.body;
+    async addEditora(req, res) {
+        try {
+            const { nome, email, telefone, data_cadastro } = req.body;
 
-        if(!nome || !email || !telefone || !data_cadastro){
-            return res.json({message: "Todos os campos são obrigatorios!"});
+            if (!nome || !email || !telefone || !data_cadastro) {
+                return res.json({ message: "Todos os campos são obrigatorios!" });
+            }
+
+            const createEditora = await editoraModel.createEditora(req.body);
+
+            if (createEditora.affectedRows === 0) {
+                return res.json({ message: "Não foi possível cadastrar a editora!" })
+            }
+
+            return res.status(201).json({ message: "Editora cadastrada com sucesso!" })
+        } catch (error) {
+            return res.json({ message: `ocorreu um erro: ${error}` })
         }
+    };
 
-        const createEditora = await editoraModel.createEditora(req.body);
-
-        if (createEditora.affectedRows === 0) {
-            return res.json({message: "Não foi possível cadastrar editora!"})
+    async updateEditora(req, res) {
+        try {
+            const id = Number(req.params.id);
+            const { nome, email, telefone, data_cadastro } = await editoraModel.updateEditora(id);
+            if (!nome || !email || !telefone || !data_cadastro) {
+                return res.json({ message: "Todos os campos são obrigatorios!" });
+            };
+            return res.json({ message: "Editora atualizada com sucesso!" });
+        } catch (error) {
+            return res.json({ message: `ocorreu um erro: ${error}` })
         }
+    };
 
-        return res.status(201).json({message: "Editora cadastrada com sucesso!"})
+    async deleteEditora(req, res) {
+        try {
+            const id = Number(req.params.id);
+            const deleteLivro = await editoraModel.deleteEditora(id);
+
+            if (deleteLivro.affectedRows === 0) {
+                return res.json({ message: "Não foi possível deletar a editora!" })
+            }
+
+            return res.status(201).json({ message: "Editora deletada com sucesso!" })
+        } catch (error) {
+            return res.json({ message: `ocorreu um erro: ${error}` })
+        }
     }
 }
