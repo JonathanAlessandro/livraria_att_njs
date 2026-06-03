4bf8a4e24c90eccd08668b87b3b8ac211bb8677f
JonathanAlessandro <jonathan.alessandro212@gmail.com>
2026-06-03 19:33:28 -0300

    ajustado 2 validações pendentes na compra

--- backend/middlewares/compraMiddleware.js
+++ backend/middlewares/compraMiddleware.js
@@ -14,6 +14,13 @@ const validateCompra = (req, res, next) => {
         errors.push("O campo quantidade é obrigatório, deve ser um número e maior que zero.");
     }
 
+    if(!valor || typeof valor !== "number" || valor <= 0) {
+        errors.push("O campo valor é obrigatório, deve ser um número e maior que zero.");
+    }
+
+    if(!desconto || typeof desconto !== "number" || desconto < 0) {
+        errors.push("O campo desconto é obrigatório, deve ser um número e não pode ser negativo.");
+    }
     if (errors.length > 0) {
         return res.status(400).json({ errors });
     }
