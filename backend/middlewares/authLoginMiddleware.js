aebf34d0e1b598589319364ca1d47727c3ddde19
JonathanAlessandro <jonathan.alessandro212@gmail.com>
2026-07-03 19:49:32 -0300

    ajuste de controle de acesso

--- backend/middlewares/authLoginMiddleware.js
+++ backend/middlewares/authLoginMiddleware.js
@@ -20,13 +20,27 @@ export const authenticationToken = (req, res, next) => {
         if (error) {
             return res.status(403).json({ error: "Token inválido!" })
         }
-
         req.user = user
-
         next()
     });
-
 }
 
+    export const adminRole = (...allowedRoles) => {
+        return (req, res, next) => {
+            if (!req.user) {
+                return res.status(401).json({
+                    error: "Usuário não autenticado!",
+                });
+            }
+
+            if (!allowedRoles.includes(req.user.role)) {
+                return res.status(403).json({
+                    error: "Você não tem permissão para realizar esta ação!",
+                });
+            }
+            next();
+        };
+
+    }
 
 
--- backend/routes/clienteRoutes.js
+++ backend/routes/clienteRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import clienteController from "../controllers/clienteController.js";
 import validateCliente from "../middlewares/clientMiddleware.js";
-import {authenticationToken} from "../middlewares/authLoginMiddleware.js";
+import {authenticationToken,adminRole} from "../middlewares/authLoginMiddleware.js";
 const routeClientes = express.Router();
 
-routeClientes.get("/",authenticationToken, clienteController.getAllClientes);
+routeClientes.get("/",authenticationToken,adminRole("admin","gerente","vendedor"),clienteController.getAllClientes);
 routeClientes.get("/email/:email", clienteController.clienteByEmail);
 
 routeClientes.get("/:id", clienteController.getClienteById);
