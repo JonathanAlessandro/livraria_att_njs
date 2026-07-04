951ba9595aa9664bf9f5ac64b6b6d8f744bc5dc0
JonathanAlessandro <jonathan.alessandro212@gmail.com>
2026-07-03 21:56:25 -0300

    versao funcional com update da imagem

--- backend/middlewares/uploadMiddleware.js
+++ backend/middlewares/uploadMiddleware.js
@@ -1,8 +1,14 @@
 import multer from "multer";
+import fs from "fs";
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "uploads/");
+        const dir = "uploads/";
+
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });       
+        }
+        cb(null, dir);
     },
     filename: (req, file, cb) => {
         cb(null, file.originalname);
