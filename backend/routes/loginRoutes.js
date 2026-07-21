7bc5e196205fe5929b465c1dcaf6a97497797147
JonathanAlessandro <jonathan.alessandro212@gmail.com>
2026-07-21 19:32:42 -0300

    adicionado a rota refresh

--- backend/routes/loginRoutes.js
+++ backend/routes/loginRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import authLoginController from "../controllers/authLoginController.js";
-import {authenticationToken}  from "../middlewares/authLoginMiddleware.js";
+import { authenticationToken } from "../middlewares/authLoginMiddleware.js";
 
 const loginRoute = express.Router();
 
 loginRoute.post("/login", authLoginController.login);
 loginRoute.post("/logout", authLoginController.logout);
-
+loginRoute.post("/refresh", authLoginController.refreshToken);
 
 export default loginRoute;
